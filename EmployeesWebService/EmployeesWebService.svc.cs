abf58748a5a11aef825219162412b1706cc81ee4
Mauricio Montoya <mauricio.montoya@mroboticsla.com>
2016-04-19 07:33:47 -0600

    [ADD] WebService para recolectar información de empleados activos
    WebService para recolectar información de empleados activos

---
+++ EmployeesWebService/EmployeesWebService.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmployeesWebService
+{
+    public class EmployeesWebService : IEmployeesWebService
+    {
+        static String mainConnectionString = "SykesVisitorsDB";
+        
+        public DataTable getEmployeesActive()
+        {
+            DataTable dataTable = new DataTable();
+            dataTable.TableName = "EMPLOYEES";
+            string connString = ConfigurationManager.ConnectionStrings[mainConnectionString].ConnectionString;
+            string query = "'11184' as 'SALID','Luis' as 'EMP_FIRST_NAME', 'Ernesto' as EMP_SECOND_NAME, 'Mendoza' as EMP_FIRST_SURNAME,'Sanchez' as EMP_SECOND_SURNAME from tbl_emp_employees";
+
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dataTable);
+            conn.Close();
+            da.Dispose();
+
+            return dataTable;
+        }
+    }
+}
---
+++ EmployeesWebService/IEmployeesWebService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmployeesWebService
+{
+    [ServiceContract]
+    public interface IEmployeesWebService
+    {
+
+        [OperationContract]
+        DataTable getEmployeesActive();
+    }
+}
