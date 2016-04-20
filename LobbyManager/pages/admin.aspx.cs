e41ccbf037608ae2f128c24ec663f882294ee23c
Mauricio Montoya <mauricio.montoya@mroboticsla.com>
2016-04-20 12:29:31 -0600

    [ADD] Validaciones para BD Vacía
    Validaciones para BD Vacía

--- LobbyManager/pages/admin.aspx.cs
+++ LobbyManager/pages/admin.aspx.cs
@@ -44,7 +44,7 @@ namespace LobbyManager.pages
             lbl7.Text = getFromDays("7");
             lblToday.Text = getFromDays("1");
             GetGraphData();
-            GraphTimer.Tick +=GraphTimer_Tick;
+            GraphTimer.Tick += GraphTimer_Tick;
         }
 
         /// <summary>
@@ -54,10 +54,13 @@ namespace LobbyManager.pages
         /// <param name="e"></param>
         protected void GraphTimer_Tick(object sender, EventArgs e)
         {
-            //read data from SQL or file
-            ScriptManager.RegisterClientScriptBlock(this, typeof(System.Web.UI.Page), "GraphDT", "GraphDT([" + morris_doctype_data + "]);", true);
-            ScriptManager.RegisterClientScriptBlock(this, typeof(System.Web.UI.Page), "GraphDP", "GraphDP([" + morris_department_data + "]);", true);
-            ScriptManager.RegisterClientScriptBlock(this, typeof(System.Web.UI.Page), "GraphWD", "GraphWD([" + morris_weekday_data + "]);", true);
+            if (!lbl_visitorsCount.Text.Equals("0"))
+            {
+                //read data from SQL or file
+                ScriptManager.RegisterClientScriptBlock(this, typeof(System.Web.UI.Page), "GraphDT", "GraphDT([" + morris_doctype_data + "]);", true);
+                ScriptManager.RegisterClientScriptBlock(this, typeof(System.Web.UI.Page), "GraphDP", "GraphDP([" + morris_department_data + "]);", true);
+                ScriptManager.RegisterClientScriptBlock(this, typeof(System.Web.UI.Page), "GraphWD", "GraphWD([" + morris_weekday_data + "]);", true);
+            }
         }
 
         /// <summary>
@@ -67,95 +70,101 @@ namespace LobbyManager.pages
         {
             try
             {
-                morris_doctype_data = "";
-                morris_department_data = "";
-                morris_weekday_data = "";
-                string connStr = ConfigurationManager.ConnectionStrings[mainConnectionString].ConnectionString;
-                
-                using (var conn = new SqlConnection(connStr))
-                using (var cmd = conn.CreateCommand())
+                morris_doctype_data = "{label:\"Sin Visitas\", value: 0}";
+                morris_department_data = "{label:\"Sin Visitas\", value: 0}";
+                morris_weekday_data = "{y:\"Sin Visitas\", a: 0, b: 0}";
+                if (!getVisitorsCount().Equals("0"))
                 {
-                    conn.Open();
-                    cmd.CommandText = "SELECT doc_id, doc_name FROM [tbl_doc_documents] where doc_status <> 0";
-                    SqlDataReader dreader = cmd.ExecuteReader();
-                    int count = 0;
-                    while (dreader.Read())
+                    morris_doctype_data = "";
+                    morris_department_data = "";
+                    morris_weekday_data = "";
+                    string connStr = ConfigurationManager.ConnectionStrings[mainConnectionString].ConnectionString;
+
+                    using (var conn = new SqlConnection(connStr))
+                    using (var cmd = conn.CreateCommand())
                     {
-                        using (var conn2 = new SqlConnection(connStr))
-                        using (var cmd2 = conn2.CreateCommand())
+                        conn.Open();
+                        cmd.CommandText = "SELECT doc_id, doc_name FROM [tbl_doc_documents] where doc_status <> 0";
+                        SqlDataReader dreader = cmd.ExecuteReader();
+                        int count = 0;
+                        while (dreader.Read())
                         {
-                            conn2.Open();
-                            cmd2.CommandText = "SELECT isnull(count(vis_doctype), 0) AS com_total FROM [tbl_vis_visitors] where vis_doctype = @vis_doctype";
-                            cmd2.Parameters.AddWithValue("vis_doctype", dreader["doc_id"].ToString());
-                            SqlDataReader dreader2 = cmd2.ExecuteReader();
-                            if (dreader2.Read())
+                            using (var conn2 = new SqlConnection(connStr))
+                            using (var cmd2 = conn2.CreateCommand())
                             {
-                                if (count > 0) morris_doctype_data += ", ";
-                                morris_doctype_data += "{label: \"" + dreader["doc_name"].ToString().Trim() + "\", value:" + dreader2["com_total"].ToString().Trim() + "}";
+                                conn2.Open();
+                                cmd2.CommandText = "SELECT isnull(count(vis_doctype), 0) AS com_total FROM [tbl_vis_visitors] where vis_doctype = @vis_doctype";
+                                cmd2.Parameters.AddWithValue("vis_doctype", dreader["doc_id"].ToString());
+                                SqlDataReader dreader2 = cmd2.ExecuteReader();
+                                if (dreader2.Read())
+                                {
+                                    if (count > 0) morris_doctype_data += ", ";
+                                    morris_doctype_data += "{label: \"" + dreader["doc_name"].ToString().Trim() + "\", value:" + dreader2["com_total"].ToString().Trim() + "}";
+                                    count++;
+                                }
+                                dreader2.Close();
+                                conn2.Close();
+                            }
+                        }
+                        dreader.Close();
+                        conn.Close();
+                    }
+
+                    using (var conn = new SqlConnection(connStr))
+                    using (var cmd = conn.CreateCommand())
+                    {
+                        conn.Open();
+                        cmd.CommandText = "SELECT dep_id, dep_name FROM [tbl_dep_departments] where dep_status <> 0";
+                        SqlDataReader dreader = cmd.ExecuteReader();
+                        int count = 0;
+                        while (dreader.Read())
+                        {
+                            using (var conn2 = new SqlConnection(connStr))
+                            using (var cmd2 = conn2.CreateCommand())
+                            {
+                                conn2.Open();
+                                cmd2.CommandText = "SELECT isnull(count(vis_department), 0) AS com_total FROM [tbl_vis_visitors] where vis_department = @vis_department";
+                                cmd2.Parameters.AddWithValue("vis_department", dreader["dep_id"].ToString());
+                                SqlDataReader dreader2 = cmd2.ExecuteReader();
+                                if (dreader2.Read())
+                                {
+                                    if (count > 0) morris_department_data += ", ";
+                                    morris_department_data += "{label: \"" + dreader["dep_name"].ToString().Trim() + "\", value:" + dreader2["com_total"].ToString().Trim() + "}";
+                                    count++;
+                                }
+                                dreader2.Close();
+                                conn2.Close();
+                            }
+                        }
+                        dreader.Close();
+                        conn.Close();
+                    }
+
+                    using (var conn = new SqlConnection(connStr))
+                    using (var cmd = conn.CreateCommand())
+                    {
+                        conn.Open();
+                        cmd.CommandText = "SET LANGUAGE Spanish;";
+                        cmd.ExecuteNonQuery();
+                        cmd.CommandText = "select distinct DATEPART(dw, vis_date), DATENAME(dw, vis_date) as 'day', " +
+                            "(select count (b.vis_date) from tbl_vis_visitors b where DATEPART(dw, b.vis_date) = DATEPART(dw, a.vis_date) and convert(time, b.vis_date, 108) < convert(time, '12:00:00', 108)) as 'am', " +
+                            "(select count (b.vis_date) from tbl_vis_visitors b where DATEPART(dw, b.vis_date) = DATEPART(dw, a.vis_date) and convert(time, b.vis_date, 108) >= convert(time, '12:00:00', 108)) as 'pm' " +
+                            "from tbl_vis_visitors a order by DATEPART(dw, vis_date) asc";
+                        SqlDataReader dreader = cmd.ExecuteReader();
+                        int count = 0;
+                        while (dreader.Read())
+                        {
+                            using (var conn2 = new SqlConnection(connStr))
+                            using (var cmd2 = conn2.CreateCommand())
+                            {
+                                if (count > 0) morris_weekday_data += ", ";
+                                morris_weekday_data += "{y: \"" + dreader["day"].ToString().Trim() + "\", a:\"" + dreader["am"].ToString().Trim() + "\", b:\"" + dreader["pm"].ToString().Trim() + "\"}";
                                 count++;
                             }
-                            dreader2.Close();
-                            conn2.Close();
                         }
+                        dreader.Close();
+                        conn.Close();
                     }
-                    dreader.Close();
-                    conn.Close();
-                }
-
-                using (var conn = new SqlConnection(connStr))
-                using (var cmd = conn.CreateCommand())
-                {
-                    conn.Open();
-                    cmd.CommandText = "SELECT dep_id, dep_name FROM [tbl_dep_departments] where dep_status <> 0";
-                    SqlDataReader dreader = cmd.ExecuteReader();
-                    int count = 0;
-                    while (dreader.Read())
-                    {
-                        using (var conn2 = new SqlConnection(connStr))
-                        using (var cmd2 = conn2.CreateCommand())
-                        {
-                            conn2.Open();
-                            cmd2.CommandText = "SELECT isnull(count(vis_department), 0) AS com_total FROM [tbl_vis_visitors] where vis_department = @vis_department";
-                            cmd2.Parameters.AddWithValue("vis_department", dreader["dep_id"].ToString());
-                            SqlDataReader dreader2 = cmd2.ExecuteReader();
-                            if (dreader2.Read())
-                            {
-                                if (count > 0) morris_department_data += ", ";
-                                morris_department_data += "{label: \"" + dreader["dep_name"].ToString().Trim() + "\", value:" + dreader2["com_total"].ToString().Trim() + "}";
-                                count++;
-                            }
-                            dreader2.Close();
-                            conn2.Close();
-                        }
-                    }
-                    dreader.Close();
-                    conn.Close();
-                }
-
-                using (var conn = new SqlConnection(connStr))
-                using (var cmd = conn.CreateCommand())
-                {
-                    conn.Open();
-                    cmd.CommandText = "SET LANGUAGE Spanish;";
-                    cmd.ExecuteNonQuery();
-                    cmd.CommandText = "select distinct DATEPART(dw, vis_date), DATENAME(dw, vis_date) as 'day', " +
-                        "(select count (b.vis_date) from tbl_vis_visitors b where DATEPART(dw, b.vis_date) = DATEPART(dw, a.vis_date) and convert(time, b.vis_date, 108) < convert(time, '12:00:00', 108)) as 'am', " +
-                        "(select count (b.vis_date) from tbl_vis_visitors b where DATEPART(dw, b.vis_date) = DATEPART(dw, a.vis_date) and convert(time, b.vis_date, 108) >= convert(time, '12:00:00', 108)) as 'pm' " +
-                        "from tbl_vis_visitors a order by DATEPART(dw, vis_date) asc";
-                    SqlDataReader dreader = cmd.ExecuteReader();
-                    int count = 0;
-                    while (dreader.Read())
-                    {
-                        using (var conn2 = new SqlConnection(connStr))
-                        using (var cmd2 = conn2.CreateCommand())
-                        {
-                            if (count > 0) morris_weekday_data += ", ";
-                            morris_weekday_data += "{y: \"" + dreader["day"].ToString().Trim() + "\", a:\"" + dreader["am"].ToString().Trim() + "\", b:\"" + dreader["pm"].ToString().Trim() + "\"}";
-                            count++;
-                        }
-                    }
-                    dreader.Close();
-                    conn.Close();
                 }
             }
             catch (Exception a)
